86ad3bc70d25a1b5a7ba516f21f6d7839755a811
sarahzini <sazini@g.jct.ac.il>
2024-11-07 12:48:53 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program7582.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Stage0
+{
+    partial class Program
+    {
+         static void Main(string[] args)
+        {
+            Welcome7582();
+            Welcome3332();
+            Console.ReadKey();
+        }
+        static partial void Welcome3332();
+        private static void Welcome7582()
+        {
+            //input the users's name
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+
+            //print message with the users's name
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
