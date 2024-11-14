f94a8d0b3caeafb7945b232068ce63bef9e12b94
sarahzini <sazini@g.jct.ac.il>
2024-11-14 19:09:25 +0200

    Adding the Volunteer entity

--- DalFacade/DO/Enums.cs
+++ DalFacade/DO/Enums.cs
@@ -1,5 +1,7 @@
 ﻿namespace DO;
+public enum Job { Volunteer, Director }
 
+public enum WhichDistance { AirDistance, WalkingDistance, DrivingDistance }
 internal class Enums
 {
 }
---
+++ DalFacade/DO/Volunteer.cs
@@ -0,0 +1,19 @@
+﻿namespace DO;
+public record Volunteer
+(
+    int Id,
+    string Name,
+    string PhoneNumber,
+    string Email,
+    string? Password=null, //faire quelque chose
+    string? Adress=null,
+    double? Latitude=null,
+    double? Longitude=null,
+    Job MyJob=Job.Volunteer,
+    bool active=false,
+    double? distance=null,
+    WhichDistance MyWhichDistance= WhichDistance.AirDistance
+)
+{
+    public Volunteer() : this(0, "", "", "") {} //empty ctor for stage 3
+}
