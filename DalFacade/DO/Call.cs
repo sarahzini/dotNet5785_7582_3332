63b38123a36d6c4ac0a227b67cd687374eb35509
sarah <sheymann@g.jct.ac.il>
2024-11-14 19:36:45 +0200

    Adding the call entity

---
+++ DalFacade/DO/Call.cs
@@ -0,0 +1,24 @@
+﻿
+namespace DO;
+
+public record Call
+(
+
+  int Id,
+  string Address,
+  double Latitude,
+  double Longitude,
+  DateTime DateTime,
+  SystemType Choice,
+  string? Description = null,
+  DateTime? EndDateTime = null
+
+)
+{
+    
+    public Call() : this(0, "", 0, 0, DateTime.Now, SystemType.food) { } //empty ctor for stage 3
+}
+
+
+
+
--- DalFacade/DO/Enums.cs
+++ DalFacade/DO/Enums.cs
@@ -2,6 +2,7 @@
 public enum Job { Volunteer, Director }
 
 public enum WhichDistance { AirDistance, WalkingDistance, DrivingDistance }
+public enum SystemType {food,tel }//to declare after selon type du project
 internal class Enums
 {
 }
