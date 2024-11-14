5d4e82724f98fe7df542d712eb5a835cb2f3fc4a
sarahzini <sazini@g.jct.ac.il>
2024-11-14 23:14:45 +0200

    Adding class DataSource

---
+++ DalList/DataSource.cs
@@ -0,0 +1,7 @@
+﻿namespace Dal;
+internal static class DataSource
+{
+    internal static List<DO.Assignment> Assignments { get; } = new();
+    internal static List<DO.Call> Calls { get; } = new();
+    internal static List<DO.Volunteer> Volunteers { get; } = new();
+}
