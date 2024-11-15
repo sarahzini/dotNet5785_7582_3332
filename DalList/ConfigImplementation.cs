394202f550b8cbbe017bd6757e7080609a9bbbd8
sarahzini <sazini@g.jct.ac.il>
2024-11-15 03:01:38 +0200

    Config interface and ConfigImplementation

---
+++ DalList/ConfigImplementation.cs
@@ -0,0 +1,28 @@
+﻿namespace Dal;
+using DalApi;
+using DO;
+using DalList;
+
+// This class implements the IConfig interface and provides access to configuration settings.
+internal class ConfigImplementation : IConfig
+{
+    // Property to get or set the Clock configuration.
+    public DateTime Clock
+    {
+        get => Config.Clock;
+        set => Config.Clock = value;
+    }
+
+    // Property to get or set the RiskRange configuration.
+    public TimeSpan RiskRange
+    {
+        get => Config.RiskRange;
+        set => Config.RiskRange = value;
+    }
+
+    // Method to reset the configuration settings.
+    public void Reset()
+    {
+        Config.Reset();
+    }
+}
