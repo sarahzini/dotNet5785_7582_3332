1190d3296798013ffdf37ccd31c2b85c19e0fc6f
sarah <sheymann@g.jct.ac.il>
2024-11-17 03:10:36 +0200

    Start of the main

--- DaleTest/Program.cs
+++ DaleTest/Program.cs
@@ -1,11 +1,13 @@
-﻿
-namespace DaleTest
+﻿using Dal;
+using DalApi;
+using DalList;
+namespace DaleTest;
+
+internal class Program
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    private static IAssignment? s_dalAssignement = new AssignmentImplementation(); //stage 1
+    private static ICall? s_dalCall = new CallImplementations(); //stage 1
+    private static IVolunteer? s_dalVolunteer = new VolunteerImplementations(); //stage 1
+    private static IConfig? s_dalConfig = new ConfigImplementation(); //stage 1
+
 }
