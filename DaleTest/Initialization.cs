13e23031102d1c8fcfe445a4f38d552518e49773
sarah <sheymann@g.jct.ac.il>
2024-11-18 19:11:04 +0200

    Uptading adresses, longitudes and latitudes on the initialization

--- DaleTest/Initialization.cs
+++ DaleTest/Initialization.cs
@@ -32,28 +32,13 @@ public static class Initialization
             "Hila.Zaken@webmail.com" };
 
 
-        string[] adresses = {
-    "12 Ben Yehuda Street, Jerusalem",
-    "45 Hillel Street, Jerusalem",
-    "67 Yafo Street, Jerusalem",
-    "101 Herzl Street, Jerusalem",
-    "23 King David Street, Jerusalem",
-    "56 Agron Street, Jerusalem",
-    "89 Shlomzion Hamalka Street, Jerusalem",
-    "32 King George Street, Jerusalem",
-    "78 Emek Refaim Street, Jerusalem",
-    "5 Derech Hevron, Jerusalem",
-    "14 Eliezer Kaplan Street, Jerusalem",
-    "33 Shmuel Hanavi Street, Jerusalem",
-    "8 Dov Hoz Street, Bnei Brak",
-    "50 Keren Hayesod Street, Jerusalem",
-    "63 Tchernichovsky Street, Jerusalem",
-    "29 Menachem Begin Street, Jerusalem",
-    "72 Malcha Street, Jerusalem",
-    "101 Sderot Yerushalayim Street, Jerusalem",
-    "120 Shalom Aleichem Street, Jerusalem",
-    "57 Har Homa Street, Jerusalem"
-};
+       
+        string[] adresses = { 
+            "12 Ben Yehuda Street, Jerusalem", "45 Hillel Street, Jerusalem", "67 Yafo Street, Jerusalem", "101 Herzl Street, Jerusalem", "23 King David Street, Jerusalem",
+            "56 Agron Street, Jerusalem", "89 Shlomzion Hamalka Street, Jerusalem", "32 King George Street, Jerusalem", "78 Emek Refaim Street, Jerusalem", "5 Derech Hevron, Jerusalem",
+            "14 Eliezer Kaplan Street, Jerusalem", "33 Shmuel Hanavi Street, Jerusalem", "8 Dov Hoz Street, Jerusalem", "50 Keren Hayesod Street, Jerusalem", "63 Tchernichovsky Street, Jerusalem",
+            "29 Menachem Begin Street, Jerusalem", "72 Malcha Street, Jerusalem", "101 Sderot Yerushalayim Street, Jerusalem", "120 Shalom Aleichem Street, Jerusalem", "57 Har Homa Street, Jerusalem"
+            };
 
         double[] latitudes = {
     31.7815951,
@@ -62,17 +47,17 @@ public static class Initialization
     31.780965,
     31.7745543,
     31.7779632,
+    31.7788813,
     31.779475,
     31.759882,
-    31.782512 ,
-    31.770842,
-    35.2017399,
+    31.779475,
+    31.779277,
     31.7898752,
+    31.756329,
+    31.7716677,
     31.7632657,
     31.777348,
     31.7943135,
-    31.7856,
-    31.7526,
     31.7797692,
     31.771344,
     31.7677499
@@ -88,10 +73,10 @@ public static class Initialization
     35.2226822,
     35.215608,
     35.178086,
-    35.225733,
+    35.2151934,
     35.2017399,
     35.2249504,
-    34.837327,
+    35.195831,
     35.2224285,
     35.2023972,
     35.212409,
@@ -136,75 +121,31 @@ public static class Initialization
     private static void createCalls()
     {
         string[] addresses =
-        { "1 HaYarkon St, Tel Aviv",
-        "2 Jaffa St, Jerusalem",
-        "3 Ben Yehuda St, Haifa",
-        "4 Herzl St, Rishon LeZion",
-        "5 Rothschild Blvd, Tel Aviv",
-        "6 King George St, Jerusalem",
-        "7 Dizengoff St, Tel Aviv",
-        "8 Allenby St, Tel Aviv",
-        "9 Ibn Gabirol St, Tel Aviv",
-        "10 Bograshov St, Tel Aviv",
-        "11 Frishman St, Tel Aviv",
-        "12 Nahalat Binyamin St, Tel Aviv",
-        "13 Florentin St, Tel Aviv",
-        "14 Montefiore St, Tel Aviv",
-        "15 Shalom Aleichem St, Tel Aviv",
-        "16 Bialik St, Tel Aviv",
-        "17 Trumpeldor St, Tel Aviv",
-        "18 Pinsker St, Tel Aviv",
-        "19 Mapu St, Tel Aviv",
-        "20 Shlomo Hamelech St, Tel Aviv",
-        "21 Nordau Blvd, Tel Aviv",
-        "22 Ben Gurion Blvd, Tel Aviv",
-        "23 Yehoshua Bin Nun St, Tel Aviv",
-        "24 Weizmann St, Tel Aviv",
-        "25 Kaplan St, Tel Aviv",
-        "26 Menachem Begin Rd, Tel Aviv",
-        "27 HaYarkon St, Tel Aviv",
-        "28 HaMasger St, Tel Aviv",
-        "29 HaRakevet St, Tel Aviv",
-        "30 HaShalom Rd, Tel Aviv",
-        "31 HaArba'a St, Tel Aviv",
-        "32 Yigal Alon St, Tel Aviv",
-        "33 HaAliya St, Tel Aviv",
-        "34 Levontin St, Tel Aviv",
-        "35 Mikveh Israel St, Tel Aviv",
-        "36 Yehuda Margoza St, Tel Aviv",
-        "37 Shabazi St, Tel Aviv",
-        "38 Abarbanel St, Tel Aviv",
-        "39 Eilat St, Tel Aviv",
-        "40 HaMered St, Tel Aviv",
-        "41 Neve Tzedek St, Tel Aviv",
-        "42 Shalma Rd, Tel Aviv",
-        "43 Herzl St, Tel Aviv",
-        "44 Lilienblum St, Tel Aviv",
-        "45 HaYarkon St, Tel Aviv",
-        "46 Jaffa St, Jerusalem",
-        "47 Ben Yehuda St, Haifa",
-        "48 Herzl St, Rishon LeZion",
-        "49 Rothschild Blvd, Tel Aviv",
-        "50 King George St, Jerusalem"
-    };
+        { "10 yafo st, Jerusalem","2 Jaffa St, Jerusalem","3 Ben Yehuda St, Jerusalem","4 Herzl St, Jerusalem","5 Azza st, Jerusalem",
+          "6 King George St, Jerusalem", "50 King George St, Jerusalem", "12 King George St, Jerusalem", "45 Jaffa St, Jerusalem",
+          "78 Ben Yehuda St, Jerusalem","23 Agron St, Jerusalem","56 Hillel St, Jerusalem","89 Shlomzion Hamalka St, Jerusalem,",
+          "34 HaNevi'im St, Jerusalem","67 Emek Refaim St, Jerusalem","90 Derech Hebron St, Jerusalem", "11 Keren Hayesod St, Jerusalem",
+          "22 Ramban St, Jerusalem, Israel","33 Gaza St, Jerusalem","44 Azza St, Jerusalem, Israel","55 Bezalel St, Jerusalem",
+          "66 Haneviim St, Jerusalem","77 Shmuel Hanagid St, Jerusalem","88 King David St, Jerusalem","99 Yaffo St, Jerusalem",
+          "100 Agripas St, Jerusalem","101 Haneviim St, Jerusalem","102 Ben Maimon St, Jerusalem","103 Keren Kayemet St, Jerusalem",
+          "104 Shatz St, Jerusalem","105 Hillel St, Jerusalem","106 Shlomzion Hamalka St, Jerusalem","107 King George St, Jerusalem",
+          "108 Jaffa St, Jerusalem","109 Ben Yehuda St, Jerusalem","110 Agron St, Jerusalem","111 Haneviim St, Jerusalem",
+          "112 Emek Refaim St, Jerusalem","113 Derech Hebron St, Jerusalem","114 Keren Hayesod St, Jerusalem","115 Ramban St, Jerusalem",
+          "116 Azza St, Jerusalem","117 Azza St, Jerusalem","118 Bezalel St, Jerusalem","119 Haneviim St, Jerusalem","120 Shmuel Hanagid St, Jerusalem",
+          "121 King David St, Jerusalem","122 Yaffo St, Jerusalem","123 Agripas St, Jerusalem","124 Haneviim St, Jerusalem"
+        };
 
-        double[] latitudes = {
-        32.080480, 31.783333, 32.815556, 31.964167, 32.065610, 31.783333, 32.075100, 32.073185, 32.080000,
-        32.079000, 32.080000, 32.062000, 32.056000, 32.062000, 32.079000, 32.072000, 32.075000, 32.075000,
-        32.080000, 32.080000, 32.090000, 32.085000, 32.085000, 32.085000, 32.070000, 32.070000, 32.080000,
-        32.065000, 32.065000, 32.070000, 32.070000, 32.070000, 32.060000, 32.062000, 32.060000, 32.055000,
-        32.062000, 32.056000, 32.056000, 32.063000, 32.061000, 32.056000, 32.055000, 32.062000, 32.080480,
-        31.783333, 32.815556, 31.964167, 32.065610, 31.783333
-    };
+        double[] latitudes = {31.753383,31.7780277,31.781655,31.7880941,31.773728,31.782715,31.7767741,31.782273,31.782449,31.7809244,31.759239,32.025681,31.7788813,
+                              31.7842556,31.759897,31.7832124,31.774514,31.774893,31.771599,31.771743,31.7844665,31.793836,31.7749251,31.7846329,31.7846329,31.7849633,
+                              31.7737395,31.7972733,31.7801538,31.7793341,31.7787378,31.7757616,31.7757616,31.7859455,31.7806127,31.7779632,31.7849633,31.759882,
+                              31.75279,31.750182,31.7741998,31.7704436,35.2116773,31.7701754,31.782949,31.7849633,31.794604,31.7740468,31.786086,31.784526,31.7852595
+                             };
 
-        double[] longitudes = {
-        34.763660, 35.216667, 34.989167, 34.804167, 34.777819, 35.216667, 34.774800, 34.768134, 34.781000,
-        34.768000, 34.769000, 34.770000, 34.767000, 34.774000, 34.768000, 34.769000, 34.768000, 34.770000,
-        34.769000, 34.780000, 34.774000, 34.774000, 34.782000, 34.789000, 34.784000, 34.789000, 34.767000,
-        34.782000, 34.780000, 34.792000, 34.784000, 34.791000, 34.772000, 34.774000, 34.770000, 34.756000,
-        34.765000, 34.767000, 34.761000, 34.764000, 34.764000, 34.764000, 34.769000, 34.769000, 34.763660,
-        35.216667, 34.989167, 34.804167, 34.777819, 35.216667
-    };
+        double[] longitudes = {35.222896,35.2257855,35.218937,35.199252,35.216547,35.217257,35.216209,35.216872,35.218715,35.2143122,35.205496,34.751687,35.2226822,
+                               35.2243949,35.215612,35.1948336,35.218761,35.2147,35.213181,35.212941,35.2194595,35.221176,35.2217823,35.2147682,35.2147682,35.2151037,
+                               35.2112687,35.1419648,35.2149897,35.2214706,35.2223413,35.2176332,35.2176332,35.2126195,35.2144134,35.2221859,35.2151037,35.2151934,
+                               35.220242,34.992038,35.2107476,35.2116773,35.2118899,35.2092297,35.2151037,35.221121,35.2223356,35.21214,35.211011,35.2152564
+                              };
 
         SystemType[] types = { SystemType.RegularAmbulance, SystemType.RegularAmbulance,
             SystemType.ICUAmbulance, SystemType.RegularAmbulance, SystemType.ICUAmbulance,
@@ -225,58 +166,19 @@ public static class Initialization
             SystemType.ICUAmbulance, SystemType.RegularAmbulance };
 
         string[] descriptions =
-{
-    "Response to a road accident.",
-    "Assistance for a fainting incident.",
-    "Cardiac arrest case.",
-    "Helping a dehydrated individual.",
-    "Child choking on food.",
-    "Severe allergic reaction.",
-    "Elderly person collapsed.",
-    "Labor contractions reported.",
-    "Sports injury during a game.",
-    "Overdose situation.",
-    "Smoke inhalation victims.",
-    "Drowning rescue.",
-    "Fever and weakness in a patient.",
-    "Cyclist injured in an accident.",
-    "Electric shock incident.",
-    "Severe bleeding from a knife accident.",
-    "Car crash involving multiple passengers.",
-    "Fractured leg during an activity.",
-    "Burn injuries from a fire.",
-    "Diabetic patient with low blood sugar.",
-    "Asthma attack during an event.",
-    "Teenager injured in an accident.",
-    "Suspected stroke reported.",
-    "Seizure episode.",
-    "Gunshot wound victim.",
-    "Motorcyclist injured in a collision.",
-    "Pregnant woman experiencing complications.",
-    "Accidental poisoning in a child.",
-    "Severe headache and confusion in a patient.",
-    "Bystander assistance for a fall.",
-    "Child injured on playground equipment.",
-    "Unconscious person found.",
-    "Severe burns from a chemical accident.",
-    "Dog bite incident requiring medical attention.",
-    "Heatstroke case.",
-    "Rescue for a stranded motorist.",
-    "Broken arm during an activity.",
-    "Choking incident.",
-    "Respiratory distress in a patient.",
-    "Pedestrian hit by a vehicle.",
-    "Fall from a ladder.",
-    "Electrocution from faulty wiring.",
-    "Allergic reaction to an insect sting.",
-    "Gunfire victim.",
-    "Emergency childbirth.",
-    "Cyclist hit by a vehicle.",
-    "Fainting during an event.",
-    "Hypothermia from exposure to cold.",
-    "Collapse due to dehydration.",
-    "Boat accident rescue."
-};
+        { "Response to a road accident.","Assistance for a fainting incident.","Cardiac arrest case.","Helping a dehydrated individual.",
+          "Child choking on food.","Severe allergic reaction.","Elderly person collapsed.","Labor contractions reported.","Sports injury during a game.",
+          "Overdose situation.","Smoke inhalation victims.","Drowning rescue.","Fever and weakness in a patient.","Cyclist injured in an accident.",
+          "Electric shock incident.","Severe bleeding from a knife accident.","Car crash involving multiple passengers.",
+          "Fractured leg during an activity.","Burn injuries from a fire.","Diabetic patient with low blood sugar.","Asthma attack during an event.",
+          "Teenager injured in an accident.","Suspected stroke reported.","Seizure episode.","Gunshot wound victim.",
+          "Motorcyclist injured in a collision.","Pregnant woman experiencing complications.","Accidental poisoning in a child.","Severe headache and confusion in a patient.",
+          "Bystander assistance for a fall.","Child injured on playground equipment.","Unconscious person found.","Severe burns from a chemical accident.",
+          "Dog bite incident requiring medical attention.","Heatstroke case.","Rescue for a stranded motorist.","Broken arm during an activity.",
+          "Choking incident.","Respiratory distress in a patient.","Pedestrian hit by a vehicle.","Fall from a ladder.","Electrocution from faulty wiring.",
+          "Allergic reaction to an insect sting.","Gunfire victim.","Emergency childbirth.","Cyclist hit by a vehicle.","Fainting during an event.",
+          "Hypothermia from exposure to cold.","Collapse due to dehydration.","Boat accident rescue."
+        };
 
         for (int i = 0; i < 50; i++)
         {
@@ -300,35 +202,35 @@ public static class Initialization
     }
     /// Create a list of assignments by randomly assigning volunteers to calls.
     private static void createAssignments()
-{
-    List<Volunteer> volunteers = s_dalVolunteer?.ReadAll() ?? new List<Volunteer>();
-    List<Call> calls = s_dalCall?.ReadAll() ?? new List<Call>();
-
-    foreach (var call in calls)
     {
+        List<Volunteer> volunteers = s_dalVolunteer?.ReadAll() ?? new List<Volunteer>();
+        List<Call> calls = s_dalCall?.ReadAll() ?? new List<Call>();
+
+        foreach (var call in calls)
+        {
             // Randomly select a volunteer
             Volunteer volunteer = volunteers[s_rand.Next(volunteers.Count)];
 
             // Generate random start time for the assignment between the start of the call and 20 minutes after
-            DateTime startTime = call.DateTime.AddMinutes(s_rand.Next(20)); 
+            DateTime startTime = call.DateTime.AddMinutes(s_rand.Next(20));
 
             // Generate random end time for the assignment
             DateTime endTime = startTime.AddMinutes(s_rand.Next(20));
             EndStatus endStatus = EndStatus.Completed;
 
-                if (endTime < call.EndDateTime)
-                {
-                    endStatus = (EndStatus)s_rand.Next(1, 3); // Randomly choose between Completed, SelfCancelled and DirectorCancelled
-                }
-                else
-                    endStatus = EndStatus.Expired;
+            if (endTime < call.EndDateTime)
+            {
+                endStatus = (EndStatus)s_rand.Next(1, 3); // Randomly choose between Completed, SelfCancelled and DirectorCancelled
+            }
+            else
+                endStatus = EndStatus.Expired;
 
             // Create the assignment
             Assignment assignment = new(0, call.Id, volunteer.Id, startTime, endTime, endStatus);
             s_dalAssignement?.Create(assignment);
-        
+
+        }
     }
-}
 
     public static void Do(IVolunteer? dalVolunteer, IAssignment? dalAssignment, ICall? dalCall, IConfig? dalConfig)
     {
