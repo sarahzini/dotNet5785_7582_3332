9e2d102d269e9aae7bde375dbc551f1019cf6cf4
sarahzini <sazini@g.jct.ac.il>
2024-12-02 10:36:25 +0200

    adding factory

--- DalFacade/DalApi/DalConfig.cs
+++ DalFacade/DalApi/DalConfig.cs
@@ -1,47 +1,63 @@
 ﻿namespace DalApi;
 using System.Xml.Linq;
- 
+
+/// <summary>
+/// DalConfig class is responsible for loading and parsing the DAL configuration from an XML file.
+/// </summary>
 static class DalConfig
 {
-   /// <summary>
-   /// internal PDS class
-   /// </summary>
-   internal record DalImplementation
-   (
-       // package/dll name
-       string Package,
-       // namespace where DAL implementation class is contained in
-       string Namespace,
-       // DAL implementation class name
-       string Class  	
-   );
- 
-   internal static string s_dalName;
-   internal static Dictionary<string, DalImplementation> s_dalPackages;
- 
-   static DalConfig()
-   {
-      XElement dalConfig = XElement.Load(@"..\xml\dal-config.xml") ?? 
-                           throw new DalConfigException("dal-config.xml file is not found");
-      
-      s_dalName = 
-         dalConfig.Element("dal")?.Value ?? throw new DalConfigException("<dal> element is missing");
- 
-      var packages = dalConfig.Element("dal-packages")?.Elements() ?? 
-                     throw new DalConfigException("<dal-packages> element is missing");
-      s_dalPackages = ( 
-                        from item in packages
-                     	let pkg = item.Value
-                     	let ns = item.Attribute("namespace")?.Value ?? "Dal"
-                     	let cls = item.Attribute("class")?.Value ?? pkg
-                     	select (item.Name, new DalImplementation(pkg, ns, cls))
-                  	   ).ToDictionary(p => "" + p.Name, p => p.Item2);
-   }
+    /// <summary>
+    /// Internal record to hold DAL implementation details.
+    /// </summary>
+    /// <param name="Package">The package/dll name.</param>
+    /// <param name="Namespace">The namespace where the DAL implementation class is contained.</param>
+    /// <param name="Class">The DAL implementation class name.</param>
+    internal record DalImplementation
+    (
+        string Package,
+        string Namespace,
+        string Class
+    );
+
+    // The name of the DAL to be used, extracted from the configuration file.
+    internal static string s_dalName;
+
+    // Dictionary to hold the DAL packages and their corresponding implementation details.
+    internal static Dictionary<string, DalImplementation> s_dalPackages;
+
+    /// <summary>
+    /// Static constructor to load and parse the dal-config.xml file.
+    /// </summary>
+    static DalConfig()
+    {
+        // Load the dal-config.xml file
+        XElement dalConfig = XElement.Load(@"..\xml\dal-config.xml") ??
+                             throw new DalConfigException("dal-config.xml file is not found");
+
+        // Extract the DAL name from the configuration
+        s_dalName =
+           dalConfig.Element("dal")?.Value ?? throw new DalConfigException("<dal> element is missing");
+
+        // Extract the DAL packages and their implementation details from the configuration
+        var packages = dalConfig.Element("dal-packages")?.Elements() ??
+                       throw new DalConfigException("<dal-packages> element is missing");
+        s_dalPackages = (
+                          from item in packages
+                          let pkg = item.Value
+                          let ns = item.Attribute("namespace")?.Value ?? "Dal"
+                          let cls = item.Attribute("class")?.Value ?? pkg
+                          select (item.Name, new DalImplementation(pkg, ns, cls))
+                           ).ToDictionary(p => "" + p.Name, p => p.Item2);
+    }
 }
- 
+
+/// <summary>
+/// Exception class for handling DAL configuration errors.
+/// </summary>
 [Serializable]
 public class DalConfigException : Exception
 {
-	public DalConfigException(string msg) : base(msg) { }
-	public DalConfigException(string msg, Exception ex) : base(msg, ex) { }
+    public DalConfigException(string msg) : base(msg) { }
+    public DalConfigException(string msg, Exception ex) : base(msg, ex) { }
 }
+
---
+++ DalFacade/DalApi/Factory.cs
@@ -0,0 +1,41 @@
+﻿namespace DalApi;
+
+/// <summary>
+/// Factory class to get the appropriate IDal implementation based on the configuration.
+/// </summary>
+public static class Factory
+{
+    /// <summary>
+    /// Gets the IDal implementation based on the configuration settings.
+    /// </summary>
+    /// <exception cref="DalConfigException">Thrown when there is an issue with the configuration or loading the assembly.</exception>
+    public static IDal Get
+    {
+        get
+        {
+            // Retrieve the DAL type from the configuration
+            string dalType = DalApi.DalConfig.s_dalName ?? throw new DalConfigException($"DAL name is not extracted from the configuration");
+
+            // Retrieve the DAL implementation details from the configuration
+            DalApi.DalConfig.DalImplementation dal = DalApi.DalConfig.s_dalPackages[dalType] ??
+                throw new DalConfigException($"Package for {dalType} is not found in packages list in dal-config.xml");
+
+            // Attempt to load the assembly containing the DAL implementation
+            try
+            {
+                System.Reflection.Assembly.Load(dal.Package ?? throw new DalConfigException($"Package {dal.Package} is null"));
+            }
+            catch (Exception ex)
+            { throw new DalConfigException($"Failed to load {dal.Package}.dll package", ex); }
+
+            // Get the type of the DAL implementation class
+            Type type = Type.GetType($"{dal.Namespace}.{dal.Class}, {dal.Package}") ??
+                throw new DalConfigException($"Class {dal.Namespace}.{dal.Class} was not found in {dal.Package}.dll");
+
+            // Retrieve the singleton instance of the DAL implementation
+            return type.GetProperty("Instance", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static)?.GetValue(null) as IDal ??
+                throw new DalConfigException($"Class {dal.Class} is not a singleton or wrong property name for Instance");
+        }
+    }
+}
+
