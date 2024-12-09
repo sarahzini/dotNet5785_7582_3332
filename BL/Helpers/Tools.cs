1db73bd3b562f8feae1eb36ade60af065e019938
sarah <sheymann@g.jct.ac.il>
2024-12-09 11:13:31 +0200

    ClockManager Tools

---
+++ BL/Helpers/Tools.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection; // to use PropertyInfo
+using System.Text; // to use StringBuilder
+
+namespace Helpers;
+internal static class Tools
+{
+    public static string ToStringProperty<T>(this T t)
+    {
+        if (t == null)
+            return string.Empty;
+
+        StringBuilder str = new StringBuilder();
+        foreach (PropertyInfo item in typeof(T).GetProperties())
+        {
+            var value = item.GetValue(t, null);
+            str.Append(item.Name + ": ");
+            if (value is not string && value is IEnumerable<T> enumerable)
+            {
+                str.AppendLine();
+                foreach (var it in enumerable)
+                {
+                    str.AppendLine(it.ToString());
+                }
+            }
+            else
+            {
+                str.AppendLine(value?.ToString());
+            }
+        }
+        return str.ToString();
+    }
+
+}
